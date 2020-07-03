b6b691f97ba411b103af361052f5016189ade907
Unknown <omax1972@gmail.com>
2020-07-03 17:00:09 +0300

    Main Activity Design

--- GeniuseeTest2/app/src/main/java/com/example/geniuseetest/MainActivity.kt
+++ GeniuseeTest2/app/src/main/java/com/example/geniuseetest/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.geniuseetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var movieLV : ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        movieLV = findViewById(R.id.movieLV)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
     }
 }
