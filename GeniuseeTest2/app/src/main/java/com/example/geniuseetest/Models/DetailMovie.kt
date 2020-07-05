047b72c822613100d9bb7646b66d1bc1596eb5df
Unknown <omax1972@gmail.com>
2020-07-05 05:36:42 +0300

    Add infinity scroll

--- GeniuseeTest2/app/src/main/java/com/example/geniuseetest/Models/DetailMovie.kt
+++ GeniuseeTest2/app/src/main/java/com/example/geniuseetest/Models/DetailMovie.kt
@@ -1,5 +1,6 @@
 package com.example.geniuseetest.Models
 
+//Detail information about movie
 data class DetailMovie(var itemFilm: ItemFilm?, var genres: String, var releaseDate: String, var directors: String) {
     var poster = itemFilm?.poster
     var name = itemFilm?.name
